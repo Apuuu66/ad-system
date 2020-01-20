71f998768af19dd3e21baa2d8e5e39387420485c
汪义贵 <466007639@qq.com>
2020-01-20 19:14:15 +0800

    ad-search

---
+++ ad-service/ad-search/src/main/java/com/guier/ad/search/AdSearchApplication.java
@@ -0,0 +1,11 @@
+package com.guier.ad.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdSearchApplication.class);
+    }
+}
